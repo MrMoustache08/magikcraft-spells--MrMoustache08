7d978f703c10bf4684bf2dc275cb0e87ecd1b396
MrMoustache08 <christian@spinnys.com>
2017-12-01 10:10:49 +0000

    commit spells

---
+++ r.ts
@@ -0,0 +1,14 @@
+const magik = magikcraft.io;
+
+function r(t = 5) {
+	const times = t * 1000 / 300;
+    let n = times;
+    magik.setTimeout(shield, 300);
+    magik.dixit('Magical Shield YO!');
+    function shield() {
+        n--;
+        const location = magik.hic();
+        const nearbyEntities = location.getWorld().getNearbyEntities(location, 8, 8, 8);
+        nearbyEntities.forEac
+    }
+}
